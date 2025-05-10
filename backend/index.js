72dd4c97b891d1728efa018ce6b80b04f6c2edbd
Roshan Kumar <roshanjaiswal.bca@gmail.com>
2025-05-11 03:06:31 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const sendOTP = require("./utils/email");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
